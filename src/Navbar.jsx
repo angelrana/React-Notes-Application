f8680b6e2cc7e39420e1b43f359ce6604726412c
Angel Thapa <ennjelo@gmail.com>
2022-11-28 23:14:57 +0530

    sidebar on progress

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -5,21 +5,64 @@ import IconButton from "@mui/material/IconButton";
 import MenuIcon from "@mui/icons-material/Menu";
 import Tabs from '@mui/material/Tabs';
 import Tab from '@mui/material/Tab';
+import Drawer from '@mui/material/Drawer';
+import ChevronLeftIcon from "@mui/icons-material/ChevronLeft";
+import ChevronRightIcon from "@mui/icons-material/ChevronRight";
+import styled from '@emotion/styled';
+const drawerWidth = 240;
 
+const Main = styled("main", { shouldForwardProp: (prop) => prop !== "open" })(
+  ({ theme, open }) => ({
+    flexGrow: 1,
+    padding: theme.spacing(3),
+    transition: theme.transitions.create("margin", {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.leavingScreen,
+    }),
+    marginLeft: `-${drawerWidth}px`,
+    ...(open && {
+      transition: theme.transitions.create("margin", {
+        easing: theme.transitions.easing.easeOut,
+        duration: theme.transitions.duration.enteringScreen,
+      }),
+      marginLeft: 0,
+    }),
+  })
+);
 
+const DrawerHeader = styled("div")(({ theme }) => ({
+  display: "flex",
+  alignItems: "center",
+  padding: theme.spacing(0, 1),
+  // necessary for content to be below app bar
+  ...theme.mixins.toolbar,
+  justifyContent: "flex-end",
+}));
 
 const Navbar = () => {
-      const [value, setValue] = React.useState("one");
+  const [value, setValue] = React.useState("one");
+    const [open, setOpen] = React.useState(false);
 
       const handleChange = (event, newValue) => {
         setValue(newValue);
   };
   
+
+   const handleDrawerOpen = () => {
+     setOpen(true);
+   };
+
+   const handleDrawerClose = () => {
+     setOpen(false);
+   };
   return (
     <Box sx={{ flexGrow: 1 }}>
-      <AppBar position="static" sx={{ pl: 3, pr: 3, pt: 1, bgcolor: "white" }}>
+      <AppBar
+        open={open}
+        position="static"
+        sx={{ pl: 3, pr: 3, pt: 1, bgcolor: "white", boxShadow: "none" }}>
         <Toolbar>
-          <Box sx={{ width: "45%" }}>
+          <Box sx={{ width: "47%", display: { xs: "none", sm: "flex" } }}>
             <Tabs
               value={value}
               onChange={handleChange}
@@ -32,8 +75,8 @@ const Navbar = () => {
           </Box>
 
           <Typography
-                      sx={{
-                position: "relative",
+            sx={{
+              position: "relative",
               display: { xs: "flex", md: "flex" },
               flexGrow: 1,
               fontFamily: "'Titillium Web', sans-serif",
@@ -48,14 +91,52 @@ const Navbar = () => {
           </Typography>
 
           <IconButton
+            aria-label="open drawer"
+            onClick={handleDrawerOpen}
             size="large"
             edge="start"
-            aria-label="menu"
-            sx={{ ml: "auto", mr: 5 }}>
+            sx={{
+              ml: "auto",
+              mr: { xs: 0, sm: 0, md: 2 },
+              ...(open && { display: "none" }),
+            }}>
             <MenuIcon />
           </IconButton>
         </Toolbar>
+        <Box sx={{ width: "100%", display: { xs: "flex", sm: "none" } }}>
+          <Tabs
+            value={value}
+            onChange={handleChange}
+            textColor="secondary"
+            indicatorColor="secondary"
+            aria-label="secondary tabs example">
+            <Tab sx={{ fontSize: "11px" }} value="one" label="WEEKLY" />
+            <Tab sx={{ fontSize: "11px" }} value="two" label="DAILY" />
+          </Tabs>
+        </Box>
       </AppBar>
+      <Drawer
+        sx={{
+          width: drawerWidth,
+          flexShrink: 0,
+          "& .MuiDrawer-paper": {
+            width: drawerWidth,
+            boxSizing: "border-box",
+          },
+        }}
+        variant="persistent"
+        anchor="left"
+        open={open}>
+        <DrawerHeader>
+          <IconButton onClick={handleDrawerClose}>
+            {theme.direction === "ltr" ? (
+              <ChevronLeftIcon />
+            ) : (
+              <ChevronRightIcon />
+            )}
+          </IconButton>
+        </DrawerHeader>
+      </Drawer>
     </Box>
   );
 }
